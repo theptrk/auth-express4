dd61c854e213a43d015653467f0a9321e10fbd73
theptrk <patrick.tran06@gmail.com>
2014-08-16 14:14:09 -0700

    Added local login functionality

--- config/passport.js
+++ config/passport.js
@@ -22,7 +22,7 @@ module.exports = function(passport){
     });
   });
 
-  // === local signup ===
+  // === LOCAL SIGNUP ===
   // use named strategies to have one for login and for signup
   // this will be used with `passport.authenticat('local-signup', {})`
   passport.use('local-signup', new LocalStrategy({
@@ -59,5 +59,27 @@ module.exports = function(passport){
       });
     });
 
+  }));
+
+  // == LOCAL LOGIN ===
+  // by default if there was no name, it would be called `local`
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  function(req, email, password, done){
+    User.findOne( {'local.email': email}, function(err, user){
+      if (err)
+        return done(err);
+
+      if (!user)
+        return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+      if (!user.validPassword(password))
+        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+
+      return done(null, user);
+    });
   }));
 };
