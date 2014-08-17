1214d657eb442e04e16480d5b8dd50ae58cd6c31
theptrk <patrick.tran06@gmail.com>
2014-08-16 18:47:33 -0700

    Stripped to facebook only logins

--- app/routes.js
+++ app/routes.js
@@ -4,29 +4,6 @@ module.exports = function(app, passport){
     res.render('index.ejs');
   });
 
-  // LOGIN
-  app.route('/login')
-    .get(function(req, res){
-      res.render('login.ejs', { message: req.flash('loginMessage')});
-    })
-    .post(passport.authenticate('local-login', {
-      successRedirect : '/profile',
-      failureRedirect : '/login',
-      failureFlash: true
-    }));
-
-  // SIGNUP
-  app.route('/signup')
-    .get(function(req, res){
-      res.render('signup.ejs', { message: req.flash('signupMessage') });
-    })
-
-    .post(passport.authenticate('local-signup', {
-      successRedirect: '/profile', // this could instead be a callback
-      failureRedirect: '/signup',
-      failureFlash: true // allow flash messages
-    }));
-
   // PROFILE
   app.route('/profile')
     // we use our logged in middleware here
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,3 @@
-// note: this is required before we initailize passport
-// load all the things we need
-var LocalStrategy = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 
 // load up the user model
@@ -25,66 +22,6 @@ module.exports = function(passport){
       done(err, user);
     });
   });
-
-  // === LOCAL SIGNUP ===
-  // use named strategies to have one for login and for signup
-  // this will be used with `passport.authenticat('local-signup', {})`
-  passport.use('local-signup', new LocalStrategy({
-      // by default, LocalStrategy uses username and password. We override here
-      usernameField : 'email',
-      passwordField : 'password',
-      passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done){
-
-      // asynchronous
-      // User.findOne wont fire unless data is sent back
-      process.nextTick(function(){
-
-        User.findOne({ 'local.email': email }, function(err, user){
-          if(err)
-            return done(err);
-
-          // if user exists, send flash message
-          if(user){
-            return done(null, false, req.flash('signupMessage', 'That email is already taken'));
-          } else {
-            var newUser = new User();
-
-            newUser.local.email = email;
-            newUser.local.password = newUser.generateHash(password);
-
-            newUser.save(function(err){
-              if(err)
-                throw err;
-              return done(null, newUser);
-            });
-          }
-        });
-      });
-    }));
-
-  // === LOCAL LOGIN ===
-  // by default if there was no name, it would be called `local`
-  passport.use('local-login', new LocalStrategy({
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true
-    },
-    function(req, email, password, done){
-      User.findOne( {'local.email': email}, function(err, user){
-        if (err)
-          return done(err);
-
-        if (!user)
-          return done(null, false, req.flash('loginMessage', 'No user found.'));
-
-        if (!user.validPassword(password))
-          return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-
-        return done(null, user);
-      });
-    }));
 
   // === FACEBOOK ===
   passport.use(new FacebookStrategy({
