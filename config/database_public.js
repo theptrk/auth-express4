140b03eb61c80b7ce11e381ae595280e038221c9
theptrk <patrick.tran06@gmail.com>
2014-08-16 18:52:40 -0700

    Created database_public.js for users of repo

--- config/database_public.js
+++ config/database_public.js
@@ -1,4 +1,4 @@
 module.exports = {
   // change this url to your own url. Get a free sandbox at compose.io;
-  'url'  : 'mongodb://authexpress4:authexpress4@kahana.mongohq.com:10082/auth-express4'
-}
+  'url'  : 'mongodb://<username>:<password>@kahana.mongohq.com:<unique>'
+};
